d8ce6fef5d4be7d858a4d6ea71a86fcb568c55f3
Mauricio Junior <mauricio.junior@gmail.com>
2016-08-25 14:52:37 -0300

    Aplicativo exemplo com o aluno
    Aplicativo exemplo

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            mostraMensagem(txtNome.Text, txtIdade.Text);
+        }
+
+        private void mostraMensagem(string nome, string idade)
+        {
+            MessageBox.Show("Nome: " + nome + " Idade: " + idade, "Erro - Aplicativo de teste", 
+                MessageBoxButton.OKCancel, MessageBoxImage.Error);
+        }
+
+        private void chkChecado_Click(object sender, RoutedEventArgs e)
+        {
+            if (chkChecado.IsChecked  == true)
+            {
+                cmdEnviar.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                cmdEnviar.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+    }
+}
